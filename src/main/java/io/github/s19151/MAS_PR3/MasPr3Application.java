aaf8f44ad9a7bb4119d28b68298bd9c749e6b5aa
= <=>
2021-10-03 18:45:34 +0200

    Dodanie projektu

---
+++ src/main/java/io/github/s19151/MAS_PR3/MasPr3Application.java
@@ -0,0 +1,13 @@
+package io.github.s19151.MAS_PR3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasPr3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasPr3Application.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/s19151/MAS_PR3/MasPr3ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.s19151.MAS_PR3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasPr3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
